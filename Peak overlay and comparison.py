9206fc6945a1cdc9b90b6bacc93ae21b58dd4fba
Stan_ockers <worminator81@gmail.com>
2024-04-29 19:27:58 +0200

    optspectrum working

--- Peak overlay and comparison.py
+++ Peak overlay and comparison.py
@@ -25,7 +25,7 @@ def plot_graph_overlay(file_path_1, file_path_2):
 
     # Plot the graph
     plt.figure(1, figsize=[13, 5])
-    plt.subplot(121)                                   #put plots next to eachother in same figure
+    plt.subplot(121)                                        #put plots next to eachother in same figure
     plt.plot(x, y, color='red', alpha= 1, label='1mW')
     plt.plot(x, z, color='green', alpha= 1, label='1mW')
     plt.xlabel('Wavelength (nm)')
@@ -33,15 +33,19 @@ def plot_graph_overlay(file_path_1, file_path_2):
     plt.title('single step')
     plt.grid(True)
     plt.legend()
-    plt.ylim(400, 3000)                                     # Set the y-axis limit for left plot
+    #plt.ylim(400, 3000)                                     # Set the y-axis limit for left plot
    
     return x, y
 
 
-def plot_graph_rescale(file_path_3, file_path_4):
+def plot_graph_rescale(file_path_3, file_path_4, file_path_5, file_path_6, file_path_7, file_path_8):
     x = []
-    y = []
-    z = []
+    y1 = []
+    y2 = []
+    y3 = []
+    y4 = []
+    y5 = []
+    y6 = []
 
     # Read data from the file
     with open(file_path_3, 'r') as file:
@@ -49,50 +53,81 @@ def plot_graph_rescale(file_path_3, file_path_4):
             if line.strip():  # Skip empty lines
                 data = line.split()
                 x.append(float(data[0]))
-                y.append(float(data[1]))
+                y1.append(float(data[1]))
 
     with open(file_path_4, 'r') as file: 
         for line in file: 
             if line.strip(): 
                 data = line.split()
-                z.append(float(data[1]))
+                y2.append(float(data[1]))
 
-    for i in range(0, len(y), 1):
-        y[i]= y[i]/100
-    
-    for i in range(0, len(z), 1):
-        z[i]= z[i]/100
+    with open(file_path_5, 'r') as file: 
+        for line in file: 
+            if line.strip(): 
+                data = line.split()
+                y3.append(float(data[1]))
+
+    with open(file_path_6, 'r') as file: 
+        for line in file: 
+            if line.strip(): 
+                data = line.split()
+                y4.append(float(data[1]))
+
+    with open(file_path_7, 'r') as file: 
+        for line in file: 
+            if line.strip(): 
+                data = line.split()
+                y5.append(float(data[1]))
+
+    with open(file_path_8, 'r') as file: 
+        for line in file: 
+            if line.strip(): 
+                data = line.split()
+                y6.append(float(data[1]))
+
+    for i in range(0, len(y4), 1):
+        y6[i]= y6[i]/8
 
     # Plot the graph
     plt.figure(1)
     plt.subplot(122)                                    #put plots next to eachother in same figure
     plt.subplots_adjust(left=0.074, bottom=0.13, right=0.97, top=0.921, wspace= 0.262, hspace=None)
-    plt.plot(x, y, color='black', label='1mW')
-    plt.plot(x, z, color='blue', label='1mW')
+    plt.plot(x, y1, color='pink', label='1mW')
+    plt.plot(x, y2, color='blue', label='1mW')
+    plt.plot(x, y3, color='green', label='1mW')
+    plt.plot(x, y4, color='red', label='1mW')
+    plt.plot(x, y5, color='yellow', label='1mW')
+    plt.plot(x, y6, color='black', label='1mW')
     plt.xlabel('Wavelength (nm)')
     plt.ylabel('Photoncounts (cumulative/100)')
     plt.title('cumulative/100')
     plt.grid(True)
     plt.legend()
-    plt.ylim(400, 3000)                                     #Set the y-axis limit for right plot                             
+    #plt.ylim(400, 3000)                                     #Set the y-axis limit for right plot                             
     
-    return x, y
+    return x, y1,
 
 
 
 #single step measurements
-file_path_1 = 'Data\sample #8\S1 signal 9\\first 1 step 0,5mW.asc'
-file_path_2 = 'Data\sample #8\S1 signal 9\\second 1 step 0,5mW.asc'
+file_path_1 = 'Data\sample #9\Bright spot\signal 4 1 step.asc'
+file_path_2 = 'Data\sample #9\Bright spot\signal 4 1 step unstable peak caught at 623.asc'
 
 #cumulative measurements 1mw
-file_path_3 = 'Data\sample #8\S1 signal 9\\third acq 100 steps 1mW.asc'
+file_path_3 = 'Data\sample #9\Bright spot\signal 4 50 steps second acq 1mW.asc'
 
-#cumulative measurements 0.5mw
-file_path_4 = 'Data\sample #8\S1 signal 9\\fourth acq 100 steps 1mW.asc'
+#cumulative measurements of 50 steps
+file_path_4 = 'Data\sample #9\Bright spot\signal 4 50 steps third acq 1mW.asc'
+file_path_5 = 'Data\sample #9\Bright spot\signal 4 50 steps fourth acq 1mW.asc'
+file_path_6 = 'Data\sample #9\Bright spot\signal 4 50 steps fifth acq 1mW.asc'
+file_path_7 = 'Data\sample #9\Bright spot\signal 4 50 steps sixth acq 1mW.asc'
+
+#cumulative measurements of 400 steps / 8
+file_path_8 = 'Data\sample #9\Bright spot\signal 4 400 steps after 10 minutes.asc'
 
 
 xdata, ydata =plot_graph_overlay(file_path_1, file_path_2)
-xdata, ydata =plot_graph_rescale(file_path_3, file_path_4)
+xdata, ydata =plot_graph_rescale(file_path_3, file_path_4, file_path_5, file_path_6, file_path_7, file_path_8)
 plt.show()
 
 
